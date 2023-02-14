8087d117cd5b46ece1ca1e69fb152784414d25dc
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-02-14 11:25:14 +0530

    ADDED: Fibonacci

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LogicalProgram
+{
+    public class program
+    {
+        public static void Main(String[] args)
+        {
+            //Fibonacci.FindFibonacci();
+            //PerfectNumber.FindThePerfectNumber();
+            //Prime.PrimeNumber();
+            //Reverse.ReverseNumber();
+            //CouponNumbers.Coupon();
+            Stopwatch.Simulate();
+        }
+    }
+
+}
